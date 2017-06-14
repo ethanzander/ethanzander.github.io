9963533055aa6fadf192b0aec6ac8609dcb36db3
Ethan Zander <Ethan@ethans-mbp.uconnect.utah.edu>
2017-06-13 21:08:35 -0600

    adding student bio

---
+++ EthanZander_files/XGtVFVUVI32.js
@@ -0,0 +1,5 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["5stgF"]); }
+
+__d('legacy:connect-xd',['XD'],(function a(b,c,d,e,f,g){b.UnverifiedXD=c('XD').UnverifiedXD;b.XD=c('XD').XD;}),3);
+__d('ArticleAuthorEngagementBlock',['csx','CSS','DOMQuery','Event','Parent','Style'],(function a(b,c,d,e,f,g,h){var i="._4lu7",j="._5zb4",k={registerClickHandler:function l(m,n,o){c('Event').listen(m,'click',function(p){if(p.target&&(c('Parent').bySelector(p.target,i)||c('Parent').bySelector(p.target,j)))return;if(n!==undefined&&c('CSS').shown(n)===false){if(o){var q=c('DOMQuery').scry(m,"._3c21"),r=c('DOMQuery').scry(m,"._6m3");if(q.length===0||r.length===0)return;var s=q[0],t=r[0];c('Style').set(s,'height','237px');c('Style').set(t,'height','179px');}c('CSS').show(n);}});}};f.exports=k;}),null);
+__d('legacy:xd-arbiter',['XdArbiter'],(function a(b,c,d,e,f,g){c('XdArbiter');}),3);
